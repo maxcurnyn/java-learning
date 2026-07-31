4fd6e6e6f13fc0a6dd404638ac2cf61e3ca8327f
maxcurnyn <curnyn@tuta.io>
2026-07-31 11:42:16 -0500

    Add practiceAddingToGitHubRepos.java

---
+++ practiceAddingToGitHubRepos.java
@@ -0,0 +1,8 @@
+public class practiceAddingToGitHubRepos {
+    public static void main(String[] args) {
+
+        System.out.println("This file was created for the sole purpose of understanding how to commit and push projects to GitHub.");
+
+    }
+
+}
