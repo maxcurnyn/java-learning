ffc3aacb550999f48229b87c3c6f53b56d42f8b2
maxcurnyn <curnyn@tuta.io>
2026-08-19 15:38:06 -0500

    Add another incomplete practice file

---
+++ numberGuessingGame.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class numberGuessingGame {
+	public static void main(String[] args) {
+
+int secretNumber = 42;
+
+Scanner input = new Scanner(System.in);
+System.out.println();
+System.out.println("I'm thinking of a number between 1 and 100.");
+System.out.println();
+System.out.print("Guess: ");
+int userGuess = input.nextInt();
+
+while (userGuess != secretNumber) {
+	if (userGuess > secretNumber) {
+		System.out.println("Too high!")
+
+} // Closes if
+	} // Closes the main method
+} // Closes the class
